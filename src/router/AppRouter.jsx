f69f696c1e3bd2b358db606d99c814c4ead6ee08
oscardenas17 <oscardenas17@gmail.com>
2022-10-15 11:41:12 -0500

    Inicio-rutas de la aplicación

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "../auth/pages/LoginPage";
+import CalendarPage from "../calendar/pages/CalendarPage";
+
+const AppRouter = () => {
+  const authStatus = "not-authenticated";
+  return (
+    <>
+      <Routes>
+        {authStatus === "not-authenticated" ? (
+          <Route path="/auth/*" element={<LoginPage />} />
+        ) : (
+          <Route path="*" element={<CalendarPage />} />
+        )}
+        <Route path="/*" element={<Navigate to="/auth/login" />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AppRouter;
